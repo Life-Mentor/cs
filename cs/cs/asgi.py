57ea29f6d38f83d9935645cbb6cafc02a4a86cfa
duck <shangaoxiangnb@gmail.com>
2023-11-14 13:31:43 +0800

    优化个人界面

--- cs/cs/asgi.py
+++ cs/cs/asgi.py
@@ -1,9 +1,4 @@
 """
-ASGI config for cs project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
