158715cb14ac7a65d026858cb1a48e0db7fda2a9
duck <shangaoxiangnb@gmail.com>
2023-11-16 22:11:53 +0800

    添加文章详情页面

--- cs/main/urls.py
+++ cs/main/urls.py
@@ -3,6 +3,7 @@ from .views import *
 
 urlpatterns = [
     path('',index.as_view(),name='index'),
-    path('me/',me.as_view(),name='me'),
+    path('aboutus/',aboutus.as_view(),name='aboutus'),
     path('article/',overall.as_view(),name='article'),
+    path('detailed/<int:detailed_id>',check.as_view(),name='detailed')
 ]
--- cs/main/views.py
+++ cs/main/views.py
@@ -2,7 +2,6 @@ import traceback
 from django.shortcuts import redirect, render, HttpResponse
 from django.urls import reverse
 from django.views import View
-import uuid, os
 from django.contrib.auth.backends import ModelBackend
 
 from utils.send_email import send_register_email
@@ -14,7 +13,7 @@ class index(View):
     def get(self,requests):
         return render(requests,'index/index.html')
 
-class me(View):
+class aboutus(View):
     def get(self,requests):
         return render(requests,'index/me.html')
 
@@ -23,3 +22,13 @@ class overall(View):
         catgorys = Catgory.objects.all()
         post_list = Post.objects.all()
         return render(requests,'article/overall.html',{'catgorys':catgorys,'post_list':post_list})
+
+class check(View):
+    def get(self,requests,detailed_id):
+        try:
+            post_list = Post.objects.filter(id=detailed_id)
+        except Exception as e:
+            return HttpResponse('未查询到此文章')
+        else:
+            return render(requests,'article/detailed.html',{'post_list':post_list})
+        
